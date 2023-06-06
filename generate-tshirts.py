07185face4f0f518597bac46412892963d28cb6d
Abel Dantas <dantas.abel@gmail.com>
2023-06-06 14:42:30 +0100

    Generate 20-ish tshirts with new colors

--- generate-tshirts.py
+++ generate-tshirts.py
@@ -14,6 +14,10 @@ font = ImageFont.truetype('opensans.ttf', 20)
 output_folder = 'output_images'
 os.makedirs(output_folder, exist_ok=True)
 
+# Load color codes from a JSON file
+with open("color_codes.json", "r") as f:
+    color_codes = json.load(f)
+
 # Load color schemes from a JSON file
 with open("color_schemes.json", "r") as f:
     color_schemes = json.load(f)
@@ -23,8 +27,8 @@ for color_scheme in color_schemes:
     output_image = Image.new("RGBA", background_image.size)
 
     # Extract tshirt and logo colors from the color scheme
-    tshirt_color = tuple(color_scheme['tshirt_color'])
-    logo_color = tuple(color_scheme['logo_color'])
+    tshirt_color = tuple(color_codes[color_scheme['tshirt']]['RGB'])
+    logo_color = tuple(color_codes[color_scheme['logo']]['RGB'])
     
     # Paste the tshirt-colored t-shirt onto the output image using the t-shirt mask
     output_image.paste(tshirt_color, mask=tshirt_mask_image)
@@ -37,7 +41,7 @@ for color_scheme in color_schemes:
 
     # Draw text on the final output image
     draw = ImageDraw.Draw(final_output)
-    text = "Tshirt: " + color_scheme['tshirt_name'] + ", Logo: " + color_scheme['logo_name']
+    text = "Tshirt: " + color_scheme['tshirt'] + ", Logo: " + color_scheme['logo']
     
     x, y = 10, final_output.height - 30  # Position of the text
     fill = "black"  # Color of the text
@@ -52,7 +56,10 @@ for color_scheme in color_schemes:
     # Draw text
     draw.text((x, y), text, font=font, fill=fill)
 
-    # Save the final output image in the output folder
-    filename = color_scheme['tshirt_name'] + '-with-' + color_scheme['logo_name'] + '.png'
-    output_path = os.path.join(output_folder, filename.replace(' ', '-'))
+    # Generate the filename
+    tshirt_filename = color_scheme['tshirt'].replace(' ', '').replace('-', '').lower()
+    logo_filename = color_scheme['logo'].replace(' ', '').replace('-', '').lower()
+
+    filename = tshirt_filename + '-' + logo_filename + '.png'
+    output_path = os.path.join(output_folder, filename)
     final_output.save(output_path)
